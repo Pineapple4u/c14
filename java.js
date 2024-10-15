e13dbbb51700dfe0ab94694fa84ee109c607da2e
Pineapple4u <165797715+Pineapple4u@users.noreply.github.com>
2024-10-15 17:50:13 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+var string = "123_abc_;/?!$"; // series of character 
+var int = 123;
+
+var x = "she said, 'I am going to the park to read 'death note' with my brother'";
+console.log(x);
+
+var a = `happy`; //backticks - used more in python than js
+console.log(typeof a);
+
+var g = String("adverse");
+
+var merged = a.concat(g, x);
+document.getElementById("display").innerHTML = merged;
+
+console.log(a[2]);
+console.log(x.length); // includes the spaces and ' ' u gave
